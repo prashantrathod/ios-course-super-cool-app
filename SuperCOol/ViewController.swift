e797395d667bf3c5b74853600a7dcace717d0675
Prashant Rathod <Prashant@prashants-imac.local>
2015-12-15 18:07:58 +0000

    Initial Commit

---
+++ SuperCOol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCOol
+//
+//  Created by Prashant Rathod on 15/12/2015.
+//  Copyright © 2015 PrashantRathod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
